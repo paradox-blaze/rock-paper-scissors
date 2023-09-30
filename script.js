86d7223f7f29f8e86d0125e99b7ba828367ee8d3
Aneesh Upadhya <aneeshupadhya234@gmail.com>
2023-10-01 01:23:50 +0530

    fixed some bugs and fixed indentation

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ function getUserChoice() {
 }
 let user_point = 0;
 let computer_point = 0;
-while (user_point != 5 || computer_point != 5) {
+console.log("Prepare yourself human,first to 3 wins!");
+while (user_point != 3 || computer_point != 3) {
     var computer_choice = getComputerChoice();
     var user_choice = getUserChoice();
-    console.log("Player point is: " + user_point, "and Computer point is: " + computer_point);
     // 0 is rock;
     // 1 is paper;
     // 2 is scissors;
@@ -42,13 +42,13 @@ while (user_point != 5 || computer_point != 5) {
             }
             else {
                 console.log("Computer chose scissors.. You lose! scissors beats paper");
-                computer_point+=1;
+                computer_point += 1;
             }
             break;
         case "scissors":
             if (computer_choice == 0) {
                 console.log("Computer chose rock.. You Lose! Rock beats scissors");
-                computer_point+=1;
+                computer_point += 1;
             }
             else if (computer_choice == 1) {
                 console.log("Computer chose paper.. You Win! Scissors beats paper");
@@ -58,7 +58,18 @@ while (user_point != 5 || computer_point != 5) {
                 console.log("Computer chose scissors.. It's a tie");
             }
             break;
-            default:
-                console.log("Invalid choice Mr.User");
+        default:
+            console.log("Invalid choice Mr.User");
+            break;
     }
+    console.log("Player point is: " + user_point + "\nComputer point is: " + computer_point);
+    if (user_point == 3 || computer_point == 3) {
+        break;
+    }
+}
+if (user_point === 3) {
+    console.log("Congratulations you beat the computer!");
+}
+else {
+    console.log("You Lost sorry!");
 }
